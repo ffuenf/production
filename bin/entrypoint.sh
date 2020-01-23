ab11de4fb919cd146bdf4a4728ce98fd163a2602
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2020-01-23 12:10:01 +0100

    docker setup

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ -z $1 ]; then
+    bin/console system:install --drop-database --create-database --basic-setup || true
     bin/console theme:compile || true
     /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
