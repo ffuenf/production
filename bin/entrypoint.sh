4fb8a9ea97b0fcb0b5c1b465e563df90f60481b7
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2020-02-06 15:26:41 +0100

    move logic from entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,9 +5,6 @@ set -e
 if [ -z $1 ]; then
     bin/console assets:install || true
     bin/console system:install --force --drop-database --create-database --basic-setup || true
-    rm -Rf var/cache
-    mkdir -p var/cache var/queue
-    php -r 'include_once "vendor/autoload.php"; echo (explode("@", PackageVersions\Versions::getVersion("shopware/core"))[0]);' > public/recovery/install/data/version
     bin/console theme:compile || true
     /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
